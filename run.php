39603b06fbef5a113a66b4d89f7b4252fee16a14
Steve Boyd <emteknetnz@gmail.com>
2024-07-07 17:38:30 +1200

    Recursively read files

--- run.php
+++ run.php
@@ -24,47 +24,55 @@ if (!file_exists($reposDir)) {
 
 // Ensure _site folder exists for building the static site
 $siteDir = __DIR__ . '/_site';
-if (!file_exists($siteDir)) {
-    mkdir($siteDir, 0777, true);
+if (file_exists($siteDir)) {
+    shell_exec("rm -rf $siteDir");
 }
+mkdir($siteDir, 0777, true);
 
-$ghrepo = 'emteknetnz/docs-test-r1';
-$repo = explode('/', $ghrepo)[1];
-$branch = 'main';
-$docsDir = 'docs';
-$docsFile = 'index.md';
+$ghrepos = [
+    'emteknetnz/docs-test-r1',
+];
 
-// Download zip file
-$zipFilePath = __DIR__ . "/zips/$repo.zip";
-if (!file_exists($zipFilePath)) {
-    $url = "https://github.com/$ghrepo/archive/refs/heads/$branch.zip";
-    $c = file_get_contents($url);
-    file_put_contents($zipFilePath, $c);
-}
+foreach ($ghrepos as $ghrepo) {
 
-// Unzip file
-$unzipPath = __DIR__ . "/repos/$repo-$branch";
-if (!file_exists($unzipPath)) {
-    $zip = new ZipArchive;
-    if ($zip->open($zipFilePath) === TRUE) {
-        $zip->extractTo($reposDir);
-        $zip->close();
-    } else {
-        echo "Failed to unzip file\n";
-        exit(1);
+    $repo = explode('/', $ghrepo)[1];
+    $branch = 'main';
+    $docsDir = 'docs'; // this should be centrally configured (i.e. in w1, not r1)
+
+    // Download zip file
+    $zipFilePath = __DIR__ . "/zips/$repo.zip";
+    if (!file_exists($zipFilePath)) {
+        $url = "https://github.com/$ghrepo/archive/refs/heads/$branch.zip";
+        $c = file_get_contents($url);
+        file_put_contents($zipFilePath, $c);
+    }
+
+    // Unzip file
+    $unzipPath = __DIR__ . "/repos/$repo-$branch";
+    if (!file_exists($unzipPath)) {
+        $zip = new ZipArchive;
+        if ($zip->open($zipFilePath) === TRUE) {
+            $zip->extractTo($reposDir);
+            $zip->close();
+        } else {
+            echo "Failed to unzip file\n";
+            exit(1);
+        }
+    }
+
+    // Loop through md files in extracted zip using glob including subdirs
+    $docsFilePaths = glob("$unzipPath/$docsDir/{*.md,**/*.md}", GLOB_BRACE);
+    foreach ($docsFilePaths as $docsFilePath) {
+        $myText = file_get_contents($docsFilePath);
+        $myHtml = Markdown::defaultTransform($myText);
+        $relativePath = str_replace("$unzipPath/$docsDir/", '', $docsFilePath);
+        $filePath = "$siteDir/$relativePath";
+        $filePath = preg_replace('/\.md$/', '.html', $filePath);
+        $fileDir = dirname($filePath);
+        if (!is_dir($fileDir)) {
+            mkdir($fileDir, 0777, true);
+        }
+        file_put_contents($filePath, $myHtml);
+        echo "File written to $filePath\n";
     }
 }
-
-// Read md docs from extracted zip
-$docsFilePath = "$unzipPath/$docsDir/$docsFile";
-if (!file_exists($docsFilePath)) {
-    echo "Docs file not found\n";
-    exit(1);
-}
-# https://github.com/michelf/php-markdown
-# https://michelf.ca/projects/php-markdown/configuration/
-$myText = file_get_contents($docsFilePath);
-$myHtml = Markdown::defaultTransform($myText);
-$filePath = "$siteDir/index.html";
-file_put_contents($filePath, $myHtml);
-echo "File written to $filePath\n";
