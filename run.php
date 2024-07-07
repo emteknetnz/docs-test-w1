312fb8326b53a42f9d7e430c5ada72f7b8a3dfde
Steve Boyd <emteknetnz@gmail.com>
2024-07-07 20:23:24 +1200

    Create index files where needed

--- run.php
+++ run.php
@@ -3,6 +3,28 @@
 require 'vendor/autoload.php';
 use Michelf\Markdown;
 
+function makeHtmlPage($html) {
+    $title = getH1($html);
+    return implode("\n", [
+        '<!DOCTYPE html>',
+        '<html>',
+        '<head>',
+        '<meta charset="utf-8">',
+        "<title>$title</title>",
+        '</head>',
+        '<body>',
+        $html,
+        '</body>',
+        '</html>',
+        '',
+    ]);
+}
+
+function getH1($html) {
+    preg_match('/<h1>(.*?)<\/h1>/', $html, $matches);
+    return $matches[1] ?? '';
+}
+
 // Check if zip is installed
 $whichZip = shell_exec('which zip');
 if (empty($whichZip)) {
@@ -61,18 +83,62 @@ foreach ($ghrepos as $ghrepo) {
     }
 
     // Loop through md files in extracted zip using glob including subdirs
-    $docsFilePaths = glob("$unzipPath/$docsDir/{*.md,**/*.md}", GLOB_BRACE);
-    foreach ($docsFilePaths as $docsFilePath) {
-        $myText = file_get_contents($docsFilePath);
-        $myHtml = Markdown::defaultTransform($myText);
-        $relativePath = str_replace("$unzipPath/$docsDir/", '', $docsFilePath);
-        $filePath = "$siteDir/$relativePath";
-        $filePath = preg_replace('/\.md$/', '.html', $filePath);
-        $fileDir = dirname($filePath);
-        if (!is_dir($fileDir)) {
-            mkdir($fileDir, 0777, true);
+    $brace = '{' . implode(',', [
+        '*.md',
+        '**/*.md',
+        '**/**/*.md',
+        '**/**/**/*.md',
+        '**/**/**/**/*.md',
+        '**/**/**/**/**/*.md',
+        '**/**/**/**/**/**/*.md',
+        '**/**/**/**/**/**/**/*.md',
+    ]) . '}';
+    $mdFilePaths = glob("$unzipPath/$docsDir/$brace", GLOB_BRACE);
+    print_r($mdFilePaths);
+    foreach ($mdFilePaths as $mdFilePath) {
+        $md = file_get_contents($mdFilePath);
+        $html = Markdown::defaultTransform($md);
+        $html = makeHtmlPage($html);
+        $relativePath = str_replace("$unzipPath/$docsDir/", '', $mdFilePath);
+        $htmlFilePath = "$siteDir/$relativePath";
+        $htmlFilePath = preg_replace('/\.md$/', '.html', $htmlFilePath);
+        $htmlFileDir = dirname($htmlFilePath);
+        if (!is_dir($htmlFileDir)) {
+            mkdir($htmlFileDir, 0777, true);
         }
-        file_put_contents($filePath, $myHtml);
-        echo "File written to $filePath\n";
+        file_put_contents($htmlFilePath, $html);
+        echo "File written to $htmlFilePath\n";
+    }
+
+    // Loop through built site folders, add in missing index.html files
+    // The html will include list of all files in the dir, including subdirs
+    $siteDirs = glob("$siteDir/{*,**/*}/", GLOB_BRACE);
+    foreach ($siteDirs as $siteDir) {
+        $siteDir = rtrim($siteDir, '/');
+        $indexHtmlFilePath = "$siteDir/index.html";
+        if (file_exists($indexHtmlFilePath)) {
+            continue;
+        }
+        $title = basename($siteDir);
+        $files = glob("$siteDir/*", GLOB_BRACE);
+        $lines = [
+            "<h1>$title</h1>",
+            '<ul>',
+        ];
+        foreach ($files as $file) {
+            if (is_dir($file)) {
+                $title = basename($file);
+            } else {
+                $contents = file_get_contents($file);
+                $title = getH1($contents) ?: $file;
+            }
+            $href = str_replace($siteDir, '', $file);
+            $href = preg_replace('/\.html$/', '', $href);
+            $lines[] = "<li><a href=\"$href\">$title</a></li>";
+        }
+        $lines[] = '</ul>';
+        $html = makeHtmlPage(implode("\n", $lines));
+        file_put_contents($indexHtmlFilePath, $html);
+        echo "Index file written to $indexHtmlFilePath\n";
     }
 }
