f0c8e2f1dca1c85e4f81f96e0b4f2608faedd4d4
Steve Boyd <emteknetnz@gmail.com>
2024-07-07 22:08:31 +1200

    Lots of improvements

--- run.php
+++ run.php
@@ -3,24 +3,37 @@
 require 'vendor/autoload.php';
 use Michelf\Markdown;
 
-function makeHtmlPage($html) {
-    $title = getH1($html);
-    return implode("\n", [
-        '<!DOCTYPE html>',
-        '<html>',
-        '<head>',
-        '<meta charset="utf-8">',
-        "<title>$title</title>",
-        '</head>',
-        '<body>',
-        $html,
-        '</body>',
-        '</html>',
-        '',
-    ]);
+function makeHtmlPage($basename, $contentHtml, $sideNavHtml, $styles) {
+    $base = strpos(__DIR__, '/home/') === 0
+        ? 'file://' . __DIR__ . '/_site/'
+        : 'https://emteknetnz.github.io/docs-test-w1/';
+    $title = getH1fromHtml($contentHtml) ?: $basename;
+    return <<<EOT
+        <!DOCTYPE html>
+        <html>
+            <head>
+            <meta charset="utf-8">
+            <base href="$base">
+            <title>$title</title>
+            <style>
+                $styles
+            </style>
+            </head>
+            <body>
+                <div class="container">
+                    <div class="sidenav">
+                        $sideNavHtml
+                    </div>
+                    <div class="content">
+                        $contentHtml
+                    </div>
+                </div>
+            </body>
+        </html>
+    EOT;
 }
 
-function getH1($html) {
+function getH1fromHtml($html) {
     preg_match('/<h1>(.*?)<\/h1>/', $html, $matches);
     return $matches[1] ?? '';
 }
@@ -51,6 +64,8 @@ if (file_exists($siteDir)) {
 }
 mkdir($siteDir, 0777, true);
 
+$styles = file_get_contents('styles.css');
+
 $ghrepos = [
     'emteknetnz/docs-test-r1',
 ];
@@ -60,6 +75,8 @@ foreach ($ghrepos as $ghrepo) {
     $repo = explode('/', $ghrepo)[1];
     $branch = 'main';
     $docsDir = 'docs'; // this should be centrally configured (i.e. in w1, not r1)
+
+    $brace = '{,*/,*/*/,*/*/*/,*/*/*/*/,*/*/*/*/*/,*/*/*/*/*/*/}';
 
     // Download zip file
     $zipFilePath = __DIR__ . "/zips/$repo.zip";
@@ -83,22 +100,11 @@ foreach ($ghrepos as $ghrepo) {
     }
 
     // Loop through md files in extracted zip using glob including subdirs
-    $brace = '{' . implode(',', [
-        '*.md',
-        '**/*.md',
-        '**/**/*.md',
-        '**/**/**/*.md',
-        '**/**/**/**/*.md',
-        '**/**/**/**/**/*.md',
-        '**/**/**/**/**/**/*.md',
-        '**/**/**/**/**/**/**/*.md',
-    ]) . '}';
-    $mdFilePaths = glob("$unzipPath/$docsDir/$brace", GLOB_BRACE);
-    print_r($mdFilePaths);
+    // $brace = makeBrace('*.md');
+    $mdFilePaths = glob("$unzipPath/$docsDir/$brace*.md", GLOB_BRACE);
     foreach ($mdFilePaths as $mdFilePath) {
         $md = file_get_contents($mdFilePath);
         $html = Markdown::defaultTransform($md);
-        $html = makeHtmlPage($html);
         $relativePath = str_replace("$unzipPath/$docsDir/", '', $mdFilePath);
         $htmlFilePath = "$siteDir/$relativePath";
         $htmlFilePath = preg_replace('/\.md$/', '.html', $htmlFilePath);
@@ -112,15 +118,15 @@ foreach ($ghrepos as $ghrepo) {
 
     // Loop through built site folders, add in missing index.html files
     // The html will include list of all files in the dir, including subdirs
-    $siteDirs = glob("$siteDir/{*,**/*}/", GLOB_BRACE);
-    foreach ($siteDirs as $siteDir) {
-        $siteDir = rtrim($siteDir, '/');
-        $indexHtmlFilePath = "$siteDir/index.html";
+    $siteSubDirs = glob("$siteDir/$brace", GLOB_BRACE);
+    foreach ($siteSubDirs as $siteSubDir) {
+        $siteSubDir = rtrim($siteSubDir, '/');
+        $indexHtmlFilePath = "$siteSubDir/index.html";
         if (file_exists($indexHtmlFilePath)) {
             continue;
         }
-        $title = basename($siteDir);
-        $files = glob("$siteDir/*", GLOB_BRACE);
+        $title = basename($siteSubDir);
+        $files = glob("$siteSubDir/*", GLOB_BRACE);
         $lines = [
             "<h1>$title</h1>",
             '<ul>',
@@ -130,15 +136,36 @@ foreach ($ghrepos as $ghrepo) {
                 $title = basename($file);
             } else {
                 $contents = file_get_contents($file);
-                $title = getH1($contents) ?: $file;
+                $title = getH1fromHtml($contents) ?: $file;
             }
-            $href = str_replace($siteDir, '', $file);
+            $href = str_replace($siteSubDir, '', $file);
             $href = preg_replace('/\.html$/', '', $href);
             $lines[] = "<li><a href=\"$href\">$title</a></li>";
         }
         $lines[] = '</ul>';
-        $html = makeHtmlPage(implode("\n", $lines));
+        $html = implode("\n", $lines);
         file_put_contents($indexHtmlFilePath, $html);
         echo "Index file written to $indexHtmlFilePath\n";
+    }
+
+    // Make side nav html for the site
+    $htmlFiles = glob("$siteDir/$brace*.html", GLOB_BRACE);
+    $sideNavHtml = '<ul>';
+    foreach ($htmlFiles as $htmlFile) {
+        $level = substr_count(str_replace($siteDir, '', $htmlFile), '/');
+        $contents = file_get_contents($htmlFile);
+        $title = getH1fromHtml($contents) ?: basename($htmlFile);
+        $href = ltrim(str_replace($siteDir, '', $htmlFile), '/');
+        $sideNavHtml .= "<li class=\"level-$level\"><a href=\"$href\">$title</a></li>";
+    }
+    $sideNavHtml .= '</ul>';
+
+    // Update all HTML content files surronding with site
+    foreach ($htmlFiles as $htmlFile) {
+        $basename = basename($htmlFile);
+        $contents = file_get_contents($htmlFile);
+        $html = makeHtmlPage($basename, $contents, $sideNavHtml, $styles);
+        file_put_contents($htmlFile, $html);
+        echo "HTML file updated $htmlFile\n";
     }
 }
